e2a7b19da465a9d1ec18ea158aaba88180f59df3
nelson jr mota <118323821+motanelson@users.noreply.github.com>
2026-07-11 17:27:44 +0100

    Add files via upload

---
+++ machine.py
@@ -0,0 +1,8 @@
+import virtual
+
+
+
+print("\033c\033[47;30m\n")
+virtual.starts()
+virtual.mains()
+virtual.ends()
---
+++ virtual.py
@@ -0,0 +1,6 @@
+def starts():
+    print("\033[47;30m\nstart virtual machine...")
+def ends():
+    print("\033[47;30m\nend virtual machine.....")
+def mains():
+    print("\033[47;30m\nmain virtual machine....")
